e6ad921415f9eb60e65aeb7c82967a4f60d226e9
Annequin Alexandre <annequin.alex@gmail.com>
2021-12-27 19:38:15 +0100

    add mongoDB connection

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'LudothequeApi';
+let db;
 const homeRoutes = require('./Routes/homeRoutes');
 const app = express();
 const router = express.Router();
 const port = 8000;
 
+MongoClient.connect(url, function (err, client) {
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
+});
+
 app.use('/', homeRoutes );
+
+app.use(express.json());
 
 app.listen(port, () =>{
     console.log("Server listen on :" + port);
